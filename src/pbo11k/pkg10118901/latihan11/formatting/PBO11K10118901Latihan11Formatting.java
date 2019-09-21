9ab5bbd4facd4d6fa58cb7285de3e23f8aed3140
Gonior <43252085+Gonior@users.noreply.github.com>
2019-09-21 23:39:02 +0700

    Dibuat : Sabtu, 21 September 2019

---
+++ src/pbo11k/pkg10118901/latihan11/formatting/PBO11K10118901Latihan11Formatting.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan11.formatting;
+
+/**
+ *
+ * @author 
+ * NAMA         : Dedi Cahya 
+ * KELAS        : IF-11-K 
+ * NIM          : 10118901 
+ * DESKRIPSI    : Mengenal formatting dalam java
+ */
+public class PBO11K10118901Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : "+i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n",i);
+        System.out.printf("%%+10d : %+10d%n",iMinus);
+        System.out.printf("%%,10d : %,10d%n",i);
+        System.out.printf("%%-10d : %-10d%n",i);
+        double f = 5675482.982;
+        System.out.println("f : "+f);
+        System.out.printf("%%f : %f%n",f);
+        System.out.printf("%%.2f : %.2f%n",f);
+        System.out.printf("%%12.2f : %12.2f%n",f);
+        System.out.printf("%%,12.2f : %,12.2f%n",f);
+    }
+    
+}
